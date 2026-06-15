ecff683484f780ee957294626848d9bbc2b89812
enzie <mulyaenziepratama@gmail.com>
2026-06-15 10:15:25 +0700

    add workflowModule at imports section

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { WorkflowModule } from './workflow/workflow.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, WorkflowModule],
   controllers: [AppController],
   providers: [AppService],
 })
