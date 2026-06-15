a4e3caf128bbdb0129f4650824ac12ed113c288a
enzie <mulyaenziepratama@gmail.com>
2026-06-15 18:24:57 +0700

    import the BullModule into the root AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { WorkflowModule } from './workflow/workflow.module';
+import { BullModule } from '@nestjs/bullmq';
+import { JobModule } from './job/job.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, WorkflowModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    WorkflowModule,
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST ?? 'localhost',
+        port: parseInt(process.env.REDIS_PORT ?? '6379'),
+      },
+    }),
+    JobModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
