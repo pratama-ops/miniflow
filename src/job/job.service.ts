c02f794eefbee9d368946e843214f64b519dbb7b
enzie <mulyaenziepratama@gmail.com>
2026-06-16 16:07:54 +0700

    feat(job): add active job quota validation per user

--- src/job/job.service.ts
+++ src/job/job.service.ts
@@ -1,15 +1,16 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bullmq';
 import { Queue } from 'bullmq';
 import { PrismaService } from '../prisma/prisma.service';
 import { TriggerJobDto } from './dto/trigger-job.dto';
+import { threadCpuUsage } from 'process';
 
 @Injectable()
 export class JobService {
   constructor(
     private prisma: PrismaService, // untuk operasi database
     @InjectQueue('workflow-execution') private queue: Queue, // inject queue yang sudah didaftarkan di module
-  ) {}
+  ) { }
 
   // Terima request trigger job, validasi workflow, buat record di DB, push ke queue
   async trigger(userId: string, dto: TriggerJobDto) {
@@ -19,6 +20,33 @@ export class JobService {
     });
 
     if (!workflow) throw new NotFoundException('Workflow tidak ditemukan');
+
+    // cek kuota job aktif user
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId }
+    });
+
+    //ambil semua workflowId milik user
+    const useWorkflow = await this.prisma.workflow.findMany({
+      where: { userId },
+      select: { id: true },
+    });
+
+    const workflowId = useWorkflow.map((w) => w.id)
+
+    // hitung job aktif berdasarkan workflowId
+    const activeJobs = await this.prisma.job.count({
+      where: {
+        workflowId: { in: workflowId },
+        status: { in: ['PENDING', 'RUNNING'] },
+      },
+    });
+
+    if (activeJobs >= user!.jobQuota) {
+        throw new BadRequestException(
+          `Quota job aktif sudah penuh (${user!.jobQuota} job). Tunggu job selesai sebelum trigger baru`
+        )
+    }
 
     // buat record Job di database dengan status PENDING
     const job = await this.prisma.job.create({
